76dbd5b73a811bddba3176c61cdf20b2e7e3dd68
Yuhua Mai <yuhua.mai@duke.edu>
2013-11-13 10:27:56 -0500

    Initial Commit

---
+++ GLSL_Teapot/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  GLSL_Teapot
+//
+//  Created by Yuhua Mai on 11/13/13.
+//  Copyright (c) 2013 Yuhua Mai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
